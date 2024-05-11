7f19296c5e15adf288d58c0d2c229df5c50b2772
Sizzlesina <www.sins604@gmail.com>
2024-05-11 12:04:08 +0330

    Handling not-found state

---
+++ 01-first-project/app/not-found.jsx
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <main className='not-found'>
+      <h1>Not found</h1>
+      <p>Unfortunatly, We could not find the requested page or resource.</p>
+    </main>
+  );
+}
+
+export default NotFound;
