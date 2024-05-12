a5b6785855240b9b11244113ae1c527621b710b3
Sizzlesina <www.sins604@gmail.com>
2024-05-12 15:03:56 +0330

    Adding backend to the project and loading state/

--- 03-routing-and-page-rendering/app/(content)/news/page.jsx
+++ 03-routing-and-page-rendering/app/(content)/news/page.jsx
@@ -1,11 +1,13 @@
-import { DUMMY_NEWS } from "@/dummy-news";
 import NewsList from "@/components/NewsList";
+import { getAllNews } from "@/lib/news";
 
-function NewsPage() {
+async function NewsPage() {
+  const news = await getAllNews();
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      <NewsList news={news} />
     </>
   );
 }
