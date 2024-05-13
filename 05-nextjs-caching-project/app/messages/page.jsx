b9fd87b732cb872f78b62f39c1669b8f2fc8bf78
Sizzlesina <www.sins604@gmail.com>
2024-05-13 14:04:24 +0330

    All the methods to handle requests and revalidate the data

--- 05-nextjs-caching-project/app/messages/page.jsx
+++ 05-nextjs-caching-project/app/messages/page.jsx
@@ -1,11 +1,15 @@
 import Messages from "@/components/messages";
+// ! import { unstable_noStore } from "next/cache";
+
+// Built-in variables
+// @ export const revalidate = 5;
+// @ export const dynamic = "force-dynamic"; // Same as the cache : "no-store"
+// @ export const dynamic = "forces-static";
 
 export default async function MessagesPage() {
-  const response = await fetch("http://localhost:8080/messages", {
-    next: {
-      revalidate: 5,
-    },
-  });
+  // ! unstable_noStore();
+
+  const response = await fetch("http://localhost:8080/messages");
   const messages = await response.json();
 
   if (!messages || messages.length === 0) {
