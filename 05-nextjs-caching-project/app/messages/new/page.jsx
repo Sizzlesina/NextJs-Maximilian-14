4c900adfe16fe4cdb0015c8e440cb06f0ff33773
Sizzlesina <www.sins604@gmail.com>
2024-05-13 14:34:13 +0330

    Now revalidate the data using revalidate tag (the unstable-cache function has a third argument)

--- 05-nextjs-caching-project/app/messages/new/page.jsx
+++ 05-nextjs-caching-project/app/messages/new/page.jsx
@@ -1,7 +1,8 @@
 import { redirect } from "next/navigation";
 
 import { addMessage } from "@/lib/messages";
-// import { revalidateTag } from "next/cache";
+// import { revalidatePath } from "next/cache";
+import { revalidateTag } from "next/cache";
 
 export default function NewMessagePage() {
   async function createMessage(formData) {
@@ -9,7 +10,8 @@ export default function NewMessagePage() {
 
     const message = formData.get("message");
     addMessage(message);
-    // revalidateTag("msg");
+    // revalidatePath("/messages");
+    revalidateTag("msg");
     redirect("/messages");
   }
 
